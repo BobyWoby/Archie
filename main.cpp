086a029012d2c1aa4df09389483d403bb9dd7936
BobyWoby <2025.kzheng@jburroughs.org>
2025-06-08 19:46:41 -0500

    fixed bug

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "SDL3/SDL_init.h"
 #include "SDL3/SDL_render.h"
 #include "SDL3/SDL_scancode.h"
-#include "archie/CommandProcessor.h"
+#include "CommandProcessor.h"
 #include "whisper.h"
 #include "SDL3/SDL_audio.h"
 
